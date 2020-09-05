8f8219859fc50072ff9a73793958fe79be1a2bd6
iaggocapitanio1 <49845024+iaggocapitanio1@users.noreply.github.com>
2020-09-05 19:05:41 -0300

    New update-0.1.0
    New update-0.1.0

--- main.py
+++ main.py
@@ -1,1 +1,67 @@
 import pygame
+import os
+import time
+import random
+import sys
+
+pygame.init()
+pygame.font.init()
+WIDTH, HEIGHT = 550, 550
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("StarWars")
+# Load images
+file_resources = "assets"
+
+try:
+    RED_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_red_small.png'))
+    GREEN_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_green_small.png'))
+    BLUE_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_blue_small.png'))
+    # player ship
+    YELLOW_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_yellow.png'))
+    # Lasers
+    RED_LASERS = pygame.image.load(os.path.join(file_resources, 'pixel_laser_red.png'))
+    GREEN_LASERS = pygame.image.load(os.path.join(file_resources, 'pixel_laser_green.png'))
+    BLUE_LASERS = pygame.image.load(os.path.join(file_resources, 'pixel_laser_blue.png'))
+    YELLOW_LASERS = pygame.image.load(os.path.join(file_resources, 'pixel_laser_yellow.png'))
+    # Background
+    BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIN.get_height(),
+                                                                                                    WIN.get_width()))
+except pygame.error:
+    print("The file in the assets folder was not found, please try to put it in the right location: " +
+          str(os.path.join(__file__, file_resources)))
+
+
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    clock = pygame.time.Clock()
+    main_font = pygame.font.SysFont("comicsans", 50)
+
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
