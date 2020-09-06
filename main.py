c5d2b1bd06ec7363480e8f29487b2f7e9711dd07
iaggocapitanio1 <49845024+iaggocapitanio1@users.noreply.github.com>
2020-09-05 21:12:37 -0300

    no need of

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ try:
     RED_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_red_small.png'))
     GREEN_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_green_small.png'))
     BLUE_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_blue_small.png'))
-    # player ship
+    # player player
     YELLOW_SPACE_SHIP = pygame.image.load(os.path.join(file_resources, 'pixel_ship_yellow.png'))
     # Lasers
     RED_LASERS = pygame.image.load(os.path.join(file_resources, 'pixel_laser_red.png'))
@@ -38,6 +38,8 @@ def main():
     lives = 5
     clock = pygame.time.Clock()
     main_font = pygame.font.SysFont("comicsans", 50)
+    player_vel = 5
+    player = Player(x=200, y=200)
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
@@ -48,6 +50,7 @@ def main():
 
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        player.draw(WIN)
         pygame.display.update()
 
     while run:
@@ -56,11 +59,47 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        keys = pygame.key.get_pressed()
+        # left
+        if keys[pygame.K_a] and (player.x - player_vel) > 0:
+            player.x -= player_vel
+        # right
+        if keys[pygame.K_d] and (player.x + player_vel + player.width) < WIN.get_width():
+            player.x += player_vel
+        # up
+        if keys[pygame.K_w] and (player.y - player_vel) > 0:
+            player.y -= player_vel
+        # down
+        if keys[pygame.K_s] and (player.y + player_vel + player.height) < WIN.get_height():
+            player.y += player_vel
 
 
 
+# An Abstract Class
+
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = list()
+        self.cool_down_counter = 0
+        self.width = 50
+        self.height = 50
+
+    def draw(self, parent):
+        parent.blit(self.ship_img, (self.x, self.y))
 
 
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super(Player, self).__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASERS
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
 
 
 if __name__ == "__main__":
